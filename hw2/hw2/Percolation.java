56848f0e3582fd25b184e9ea5150aca1d9eb0d46
cjzz12345 <927147709@qq,com>
2022-06-27 12:18:09 +0800

    HW2 first complete

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,67 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private WeightedQuickUnionUF sites;
+    private int[][] open;
+    private int len;
+    private int numOpen;
+    public Percolation(int N) {
+        sites = new WeightedQuickUnionUF(N * N);
+        open = new int[N][N];
+        len = N;
+        numOpen = 0;
+    }
+
+    private int inMatrix(int row,int col) {
+        return len * row + col;
+    }
+    public void open(int row,int col) {
+        open[row][col] = 1;
+        numOpen++;
+        if (row >= 1) {
+            if (isOpen(row - 1,col))
+                sites.union(inMatrix(row,col),inMatrix(row - 1,col));
+        }
+        if (row < len - 1) {
+            if (isOpen(row + 1,col))
+                sites.union(inMatrix(row,col),inMatrix(row + 1,col));
+        }
+        if (col >= 1) {
+            if (isOpen(row,col - 1))
+                sites.union(inMatrix(row,col),inMatrix(row,col - 1));
+        }
+        if (col < len - 1) {
+            if (isOpen(row,col + 1))
+                sites.union(inMatrix(row,col),inMatrix(row,col + 1));
+        }
+    }
+
+    public boolean isOpen(int row,int col) {
+        return open[row][col] == 1;
+    }
+
+    public boolean isFull(int row,int col) {
+        if (isOpen(row,col)) {
+            for (int i = 0;i < len;i++) {
+                if (isOpen(0,i) && sites.connected(inMatrix(row,col),inMatrix(0,i)))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public int numberOfOpenSites() {
+        return numOpen;
+    }
+
+    public boolean  percolates() {
+        for (int i = 0;i < len;i++) {
+            if (isFull(len - 1,i))
+                return true;
+        }
+        return false;
+    }
+
+
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,65 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 public class PercolationStats {
+    private Percolation sites;
+    private int num;
+    private double[] sample;
+
+    public PercolationStats(int N,int T,PercolationFactory pf) {
+        num = T;
+        sample = new double[T];
+        int total,row,col;
+
+        for (int i = 0;i < T;i++) {
+            sites = pf.make(N);
+
+            while (!sites.percolates()) {
+                total = StdRandom.uniform(N * N);
+                row = total / N;
+                col = total % N;
+                if (sites.isOpen(row,col)) {
+                    sites.open(row,col);
+                }
+
+            }
+            sample[i] = (double) sites.numberOfOpenSites() / (N * N);
+        }
+
+
+    }
+
+    public double mean() {
+        double sum = 0.0;
+        for (double s : sample) {
+            sum += s;
+        }
+        return sum / num;
+    }
+
+    public double stddev() {
+        double std = 0.0;
+        double mean = mean();
+        for (double s : sample) {
+            std += (s - mean) * (s - mean);
+        }
+        std /= num - 1;
+        return Math.sqrt(std);
+    }
+
+    public double confidenceLow() {
+        double mean = mean();
+        double std = stddev();
+        return mean - (1.96 * std / Math.sqrt(num));
+    }
+
+    public double confidenceHigh() {
+        double mean = mean();
+        double std = stddev();
+        return mean + (1.96 * std / Math.sqrt(num));
+    }
+
 
 }
