ea765f8187b84494711f9500484941829e3f8f32
cjzz12345 <927147709@qq,com>
2022-07-04 18:09:50 +0800

    lab9 first complete

--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -53,19 +53,26 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        int hashCode = hash(key);
+        int position = hashCode % buckets.length;
+        return buckets[position].get(key);
     }
 
     /* Associates the specified value with the specified key in this map. */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (!containsKey(key)) {
+            size++;
+        }
+        int hashCode = hash(key);
+        int position = hashCode % buckets.length;
+        buckets[position].put(key,value);
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
